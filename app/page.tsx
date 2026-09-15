ff9c68c394644b18086c3f9986a9d99eb16be512
genicole77 <genicole77@gmail.com>
2026-09-14 21:50:20 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -732,7 +732,7 @@ export default function DatalinkGoApp() {
           className="block text-center w-full py-2.5 px-4 rounded-xl font-bold text-sm bg-gradient-to-r from-red-600 to-orange-500 text-white shadow-md hover:opacity-95 transition-opacity"
         >
           Registrar Dominio
-      <input
+     <input
             type="text"
             className="w-full px-4 py-3 rounded-xl border text-sm transition-all focus:outline-none"
             id="global-custom-affiliate-input"
@@ -783,5 +783,5 @@ export default function DatalinkGoApp() {
         </div>
       </div>
     </section>
-  </div>
+  );
 }
