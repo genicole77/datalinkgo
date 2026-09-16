c44be1810df016202e47e6cbbfc143f28f3a1b46
genicole77 <genicole77@gmail.com>
2026-09-16 16:15:31 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -713,7 +713,7 @@ const [productFilesList, setProductFilesList] = useState<File[]>([]);
    ${productFilesList.map(file => {
   const url = URL.createObjectURL(file);
   if (file.type.startsWith('video/')) {
-    return `<video src="${url}" class="w-32 h-32 object-cover rounded-lg m-1" controls></video>`;
+ return `<video src="${url}" class="w-32 h-32 object-cover rounded-lg m-1" controls playsinline muted></video>`;
   } else {
     return `<img src="${url}" class="w-32 h-32 object-cover rounded-lg m-1" alt="Media del producto" />`;
   }
